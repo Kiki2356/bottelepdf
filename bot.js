427ea8f04239f67b589f6d3d378b48ebd638a806
Kiki2356 <49116646+Kiki2356@users.noreply.github.com>
2023-06-13 00:02:31 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 // Mengambil token bot dari environment variable
 const botToken = process.env.BOT_TOKEN;
-const bot = new TelegramBot(botToken, { polling: true });
+const bot = new TelegramBot(botToken);
 
 // Fungsi untuk menyimpulkan teks dari file PDF
 async function summarizePDF(filePath) {
@@ -13,22 +13,22 @@ async function summarizePDF(filePath) {
   const options = {};
   const data = await pdfExtract.extract(filePath, options);
   const pages = data.pages;
-  
+
   let summary = '';
-  
+
   for (let i = 0; i < pages.length; i++) {
     const page = pages[i];
     const content = page.content;
-    
+
     for (let j = 0; j < content.length; j++) {
       const block = content[j];
-      
+
       if (block.str) {
         summary += block.str + ' ';
       }
     }
   }
-  
+
   return summary;
 }
 
@@ -41,7 +41,7 @@ bot.onText(/\/summarize/, (msg) => {
 // Fungsi untuk menangani pesan yang diterima
 bot.on('document', async (msg) => {
   const chatId = msg.chat.id;
-  
+
   try {
     const fileLink = await bot.getFileLink(msg.document.file_id);
     const filePath = `./input.pdf`;
@@ -60,7 +60,6 @@ bot.on('document', async (msg) => {
     bot.sendMessage(chatId, 'Terjadi kesalahan saat menyimpulkan teks dari file PDF.');
   }
 });
-
 
 // Start listening for updates
 bot.startPolling();
