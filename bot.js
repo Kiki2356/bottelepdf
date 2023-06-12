ef6867a523200f90e179b56a82c536f69b3ff0ad
Kiki2356 <49116646+Kiki2356@users.noreply.github.com>
2023-06-12 10:25:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,85 +1,63 @@
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
-const { PDFExtract } = require('pdf.js-extract');
+const PDFExtract = require('pdf.js-extract').PDFExtract;
 
 // Mengambil token bot dari environment variable
 const botToken = process.env.BOT_TOKEN;
 const bot = new TelegramBot(botToken, { polling: true });
 
-// Fungsi untuk menangani perintah /pdf
-bot.onText(/\/pdf/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Silakan kirim file PDF untuk disederhanakan teksnya.');
-});
+// Fungsi untuk menyimpulkan teks dari file PDF
+async function summarizePDF(filePath) {
+  const pdfExtract = new PDFExtract();
+  const options = {};
+  const data = await pdfExtract.extract(filePath, options);
+  const pages = data.pages;
+  
+  let summary = '';
+  
+  for (let i = 0; i < pages.length; i++) {
+    const page = pages[i];
+    const content = page.content;
+    
+    for (let j = 0; j < content.length; j++) {
+      const block = content[j];
+      
+      if (block.str) {
+        summary += block.str + ' ';
+      }
+    }
+  }
+  
+  return summary;
+}
 
-// Fungsi untuk menangani perintah /text
-bot.onText(/\/text/, (msg) => {
+// Fungsi untuk menangani perintah /summarize
+bot.onText(/\/summarize/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Silakan kirim teks untuk disederhanakan.');
+  bot.sendMessage(chatId, 'Silakan kirim file PDF untuk disimpulkan teksnya.');
 });
 
 // Fungsi untuk menangani pesan yang diterima
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
 
   // Cek jika pesan berisi file PDF
-  if (msg.document && msg.document.mime_type === 'application/pdf') {
-    const fileId = msg.document.file_id;
-    const filePath = `./${fileId}.pdf`;
+  if (msg.document) {
+    const fileLink = await bot.getFileLink(msg.document.file_id);
+    const filePath = `./input.pdf`;
 
-    bot.downloadFile(fileId, filePath)
-      .then(() => {
-        extractTextFromPDF(filePath)
-          .then((text) => {
-            summarizeText(text)
-              .then((summary) => {
-                bot.sendMessage(chatId, summary);
-              })
-              .catch(() => {
-                bot.sendMessage(chatId, 'Terjadi kesalahan saat menyederhanakan teks.');
-              });
-          })
-          .catch(() => {
-            bot.sendMessage(chatId, 'Terjadi kesalahan saat mengekstraksi teks dari PDF.');
-          });
-      })
-      .catch(() => {
-        bot.sendMessage(chatId, 'Terjadi kesalahan saat mengunduh file PDF.');
-      });
-  } else {
-    const text = msg.text;
-    summarizeText(text)
-      .then((summary) => {
-        bot.sendMessage(chatId, summary);
-      })
-      .catch(() => {
-        bot.sendMessage(chatId, 'Terjadi kesalahan saat menyederhanakan teks.');
-      });
+    // Unduh file PDF
+    const response = await fetch(fileLink);
+    const buffer = await response.buffer();
+    fs.writeFileSync(filePath, buffer);
+
+    try {
+      const summary = await summarizePDF(filePath);
+      bot.sendMessage(chatId, summary);
+    } catch (error) {
+      console.error(error);
+      bot.sendMessage(chatId, 'Terjadi kesalahan saat menyimpulkan teks dari file PDF.');
+    } finally {
+      fs.unlinkSync(filePath); // Hapus file PDF setelah digunakan
+    }
   }
 });
-
-function extractTextFromPDF(filePath) {
-  return new Promise((resolve, reject) => {
-    const pdfExtract = new PDFExtract();
-    pdfExtract.extract(filePath, {}, (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const pages = data.pages.map((page) => page.content);
-        const text = pages.join(' ');
-        resolve(text);
-      }
-    });
-  });
-}
-
-function summarizeText(text) {
-  return new Promise((resolve) => {
-    // Implementasikan algoritma penyederhanaan teks sesuai kebutuhan
-    // Contoh: Mengambil 100 karakter pertama
-    const summary = text.substring(0, 100);
-    resolve(summary);
-  });
-}
-
-console.log('Bot is running...');
